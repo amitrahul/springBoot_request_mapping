d176bc568fd96c09302927afae736258e5f1f3d4
Rahul Raj <raj.rahul686@gmail.com>
2024-12-23 02:51:31 +0530

    created employee entity and connect db using jpa, lombok

--- src/main/java/com/backendapi/springbootcrud/springbootcrud/controllers/EmployeeController.java
+++ src/main/java/com/backendapi/springbootcrud/springbootcrud/controllers/EmployeeController.java
@@ -1,6 +1,8 @@
 package com.backendapi.springbootcrud.springbootcrud.controllers;
 
 import com.backendapi.springbootcrud.springbootcrud.dto.EmployeeDTO;
+import com.backendapi.springbootcrud.springbootcrud.entites.EmployeeEntity;
+import com.backendapi.springbootcrud.springbootcrud.repositories.EmployeeRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -18,26 +20,35 @@ import java.util.List;
 @RequestMapping("/employees")
 public class EmployeeController {
 
+    private final EmployeeRepository employeeRepository ;
+
+    public EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
     //    @GetMapping(path = "/getSecretMessage")
 //    public String getEmployeeSecretMessage(){
 //        return  "secret message : hike increased to shdjs";
 //    }
     @GetMapping(path = "/{employeeId}")
-    public EmployeeDTO getEmployeeById(@PathVariable(name = "employeeId") Long id) {
-        return new EmployeeDTO(id, "raj", "raj@gmail.com", 27, LocalDate.of(2024, 1, 2), true);
+    public EmployeeEntity getEmployeeById(@PathVariable(name = "employeeId") Long id) {
+//        return new EmployeeDTO(id, "raj", "raj@gmail.com", 27, LocalDate.of(2024, 1, 2), true);
+        return employeeRepository.findById(id).orElse(null);
     }
 
 //    @GetMapping(path = "/employees")
     @GetMapping
-    public String getAllEmployee(@RequestParam(required = false , name = "inputAge") Integer age,
-                                 @RequestParam(required = false) String sortBy) {
-        return "age is " + age + " " + sortBy;
+    public List<EmployeeEntity> getAllEmployee(@RequestParam(required = false , name = "inputAge") Integer age,
+                                               @RequestParam(required = false) String sortBy) {
+//        return "age is " + age + " " + sortBy;
+        return employeeRepository.findAll();
     }
 
     @PostMapping
-    public EmployeeDTO createNewEmployee(@RequestBody EmployeeDTO employeeInput){
-        employeeInput.setId(120L);
-        return employeeInput;
+    public EmployeeEntity createNewEmployee(@RequestBody EmployeeEntity employeeInput){
+//        employeeInput.setId(120L);
+//        return employeeInput;
+        return employeeRepository.save(employeeInput);
     }
 
     @PutMapping
---
+++ src/main/java/com/backendapi/springbootcrud/springbootcrud/entites/EmployeeEntity.java
@@ -0,0 +1,56 @@
+package com.backendapi.springbootcrud.springbootcrud.entites;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+/*
+*  @Entity annotation :-
+*   -> It will tell the spring JPA or (Hibernate) that this is the java class
+*       that need tp convert the table inside database.
+*
+* ==================================>   <==================================> ==================================>
+* The @Entity annotation in Spring and Java Persistence API (JPA) is used to mark
+* a class as a persistent entity, meaning it represents a table in a relational database.
+* This is a fundamental part of the ORM (Object-Relational Mapping) paradigm,
+* where Java objects are mapped to database tables.
+-> Key Points of @Entity:
+• Class-Level Annotation
+• Primary Key
+• Automatic Table Mapping
+*
+*
+*
+* * ==================================>   <==================================> ==================================>
+                                     JPARepository Interface
+
+* The JPARepository interface in Spring Data JPA provides a set of CRUD (Create, Read, Update, Delete)
+*  operations and query methods for interacting with the database.
+Key Points of CrudRepository :
+• Generic Interface
+• Predefined Methods
+• Custom Queries
+
+ * */
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmployeeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String email;
+    private Integer age;
+    private LocalDate dateOfJoining;
+    private Boolean isActive;
+
+}
